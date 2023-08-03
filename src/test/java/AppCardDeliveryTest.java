3d0ffc5db51702456ac3698767e74ea78646a5c7
Vasily Ermolaev <vasily.erm@gmail.com>
2023-08-03 22:55:47 +0300

    Add test

--- src/test/java/AppCardDeliveryTest.java
+++ src/test/java/AppCardDeliveryTest.java
@@ -1,42 +1,26 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
+import org.junit.jupiter.api.Test;
+
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 
 public class AppCardDeliveryTest {
-
-    private WebDriver driver;
-
-    @BeforeAll
-    public static void setupAll() {
-        WebDriverManager.chromedriver().setup();
-    }
-
-    @BeforeEach
-    public void beforeEach() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--no-sandbox");
-//        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
-        driver.get("http://localhost:9999");
-    }
-
-    @AfterEach
-    public void afterEach() {
-        driver.quit();
-        driver = null;
-    }
-
     @Test
     public void appCardDeliveryPositiveTest() {
+        open("http://localhost:9999");
         $("[data-test-id='city'] .input__control").setValue("Санкт-Петербург");
-
+//        $("[data-test-id='date'] .input__control").setValue("06082023");
+        $("[data-test-id='name'] .input__control").setValue("Иванов Иван");
+        $("[data-test-id='phone'] .input__control").setValue("+79111231212");
+        $("[data-test-id='agreement'] .checkbox__box").click();
+        $(".button__text").click();
+        $(byText("Успешно!")).shouldBe(visible,Duration.ofSeconds(15));
+        $(byText("Встреча успешно забронирована на")).shouldBe(visible);
     }
 }
